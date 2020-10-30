3d8974141ac2cca691c96fadde42f67f02c7094b
SaltyBuckets <49991711+SaltyBuckets@users.noreply.github.com>
2020-10-30 20:14:22 +0530

    add arrow

--- shapes.js
+++ shapes.js
@@ -53,8 +53,15 @@ class Arrow {
   }
   
   show() {
+    var offset = 16;
     strokeWeight(this.stroke);
     line(this.bx, this.by, this.lx, this.ly);
+     push(); //start new drawing state
+     var angle = atan2(this.by - this.ly, this.bx - this.lx); //gets the angle of the line
+     translate(this.bx, this.ly); //translates to the destination vertex
+     rotate(angle - HALF_PI); //rotates the arrow point
+     triangle(-offset * 0.5, offset, offset * 0.5, offset, 0, -offset / 2); //draws the arrow point as a triangle
+     pop();
   }
 }
 
