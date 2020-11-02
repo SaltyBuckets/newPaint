0d469185fa0d119f65889c59d68063fba2caff56
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2020-11-02 21:49:48 +0530

    jdwoij

--- shapes.js
+++ shapes.js
@@ -115,7 +115,7 @@ class Paragraph {
       stroke(this.color);
       strokeWeight(1);
       drawingContext.setLineDash([6]);
-      rect(this.bx - 5, this.by - 30, this.bx + 15*(this.text.length) + 20, this.by + 20);
+      rect(this.bx - 5, this.by - 30, this.bx + 20*(this.text.length) + 20, this.by + 20);
       fill(this.color);
       noStroke();
       drawingContext.setLineDash([]);
